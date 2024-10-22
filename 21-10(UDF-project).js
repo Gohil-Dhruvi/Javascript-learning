82ab878a85e1e35003259d4bc27d25d8a1f890cc
Gohil Dhruvi <gohildhruvi168529@gmail.com>
2024-10-21 14:10:40 -1200

    21-10(UDF-project).js

---
+++ 21-10(UDF-project).js
@@ -0,0 +1,95 @@
+
+//     #1 write a function for factorial number for given number using recursion function
+
+//     function factorial(n)
+//    {
+//     if(n==1)
+//         {
+//             return 1;
+//         }
+//         else
+//         {
+//             return n*factorial(n-1);
+//         } 
+//    }
+//     console.log("factorial is : ",factorial(7));
+
+//     exp : n = 7 ; n*factorial(n-1) ==> 5040
+
+//     #2 write a function convert feet into meter
+
+//     function convertfeetintometer(feet)
+//     {
+//         let meters = feet*(0.3048);
+//         return meters;
+//     }
+//    console.log( convertfeetintometer(16));
+
+//    exp : feet =16;feet*(0.3048) ==> 4.8768
+
+//    #3 write a function find sum of natural numbers up to n (with agrument with return type) 
+   
+//    function sum(n)
+//    {
+//     if(n<1)
+//         {
+//             return 0;
+//         }
+//         else
+//         {
+//             return (n*(n+1))/2;
+//         }
+//     }
+//     console.log(sum(15));
+
+//     exp: n=15 ==> (n*(n+1))/2 ==> 120
+
+//     #4 write a function find a maximum of three number.(argument passed)
+
+//     function max(a,b,c)
+//     {
+//         let d;
+//         if(a >= b && a >= c)
+//         {
+//             console.log("a is maximum number",d = a); 
+//         }
+//         else if(b >= a && b >= c)
+//         {
+//             console.log("b is maximum number",d = b); 
+//         }
+//         else
+//         {
+//             console.log("c is maximum number",d = c); 
+//         }
+//     }
+//     max(5,1740,105)
+
+//     exp:a=5;b=100;c=15 ==> b is maximum number 1740
+
+//     #5 write a function find average of number up to n 
+
+//     function average(n)
+//     {
+//         if(n < 1)
+//         {
+//             console.log(0);
+//             return;
+//         }
+//         let s=(n*(n+1))/2;
+//         let average = s/n;
+//         console.log(average);
+//     }
+//     average(8);
+
+//     exp: average 4.5
+
+     
+
+
+
+
+
+
+
+   
+    
