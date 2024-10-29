2b084a70005a5bb8e493fbf486e8440082f39d6a
Gohil Dhruvi <gohildhruvi168529@gmail.com>
2024-10-29 14:02:03 +0530

    revision(29-10).js

---
+++ 29-10.js
@@ -0,0 +1,212 @@
+// let number=[1,2,3,4]; //primitive datatype (String , Nmuber , Boolean , Null(object) ,Undefined , Symbol)
+// console.log(number);
+
+
+// let student = {
+//     name: "Dhruvi",
+//     age : 20 
+// }; 
+// console.log(student);// non primitive datatype (Array & Object)
+
+// alert("Congrats you learning javascript"); //alert for notifaction
+
+// 3 types variable:-
+
+// let a = 15;
+// a = 10;
+// console.log(a); //Reassigned but no Redeclare 
+
+// const a = 10;
+// console.log(a); // no Reassigned no Redeclare
+
+// var a = 10;
+// var a = 15;
+// a = 20;
+// console.log(a);
+
+// {
+//     var a = 10;
+//     console.log(a); //10
+//     {
+//         var a= 30;
+//         console.log(a); //30
+//     }
+//     console.log(a); //30 
+// } // global scope
+
+// {
+//     let a = 10;
+//     console.log(a); //10
+//     {
+//         let a= 30;
+//         console.log(a); //30
+//     }
+//     console.log(a); //10
+// } block scope
+
+// var a;
+// var a=10; // global variable
+
+// const name ="xyz";
+// let newName ="abc";
+// var Name3 ="pqr";
+// console.log(name);
+// console.log(newName);
+// console.log(Name3);
+
+// let money = ["apple","banana","papya","Mango"];
+
+// let honey = {
+//     name:"Dhruvi",
+//     age:20,
+//     city:"surat"
+// };
+// console.log(honey);
+
+// let money = null
+// console.log(money);
+
+// Arithmatic oprators (+ ,- ,* ,/ ,% ,** ,++ ,--):-
+
+// let a=20;
+// let b=30;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// let c = 3;
+// let d = 4;
+
+// console.log(c**d);
+// console.log(c++); // show 3 but increment 4 // post
+// console.log(++c); // 4 // pre
+// console.log(c--); // show 3 but decrement 2 // post
+// console.log(--c); // 2 // pre
+// console.log(d++); // show 4 but increment 5 // post
+// console.log(++d); // 5 // pre
+// console.log(d--); // show 4 but decrement 3 // post
+// console.log(--d); // 3 // pre
+
+// let a = 10 , b;
+// a = a++; // 10
+// b = ++a; // 11
+
+// console.log("a => ",a); // 10 
+// console.log("b => ",b); // 11 
+
+// Assignment Operators (= ,+= ,-=,*= ,/= ,%= ,*==):-
+
+// let j = 100;
+
+// console.log(j += 5); // 105
+// console.log(j -= 5); // 95
+// console.log(j /= 5); // 20
+// console.log(j %= 5); // 0 
+// console.log(j *= 5); // 500
+// console.log(j **= 2); // 10000
+
+// Relational operator (Compression) (> ,< ,>= ,<= ,== ,=== ,!= ,!==):-
+
+// = => assign
+// == => value compare
+// ===  => value & type compare
+
+// Logical operator (&& , || , !)
+
+// if else statement :- 
+
+// let number = 2;
+
+// if(number % 2 == 0)
+// {
+//     console.log("this is even number");
+// }
+// else
+// {
+//     console.log("this is odd number");
+// }
+
+//  ladder if-else statement 
+// let marks = -10;
+
+// if(marks < 0 || marks > 100)
+// {
+//     console.log("Enter valid marks, marks should be in between 0-100");   
+// }
+// else if(marks > 90 && marks <= 100)
+// {
+//     console.log("Congratulations you have got A+ grade");  
+// }
+// else if(marks>80 && marks <= 90)
+// {
+//     console.log("Congratulations you have got A grade");  
+// }
+// else if(marks > 70 && marks <= 80)
+// {
+//     console.log("Congratulations you have got B grade");  
+// }
+// else if(marks>60 && marks <= 70)
+// {
+//     console.log("Congratulations you have got C grade");  
+// }
+// else if(marks>50 && marks <= 60)
+// {
+//     console.log("Congratulations you have got D grade,you need to work hard");  
+// }
+// else
+// { 
+//     console.log("Congratulations you are going to study in same class again , you are fail");
+// }
+
+// nested if-else statement :-
+
+// let user = "gohildhruvi1685@gmail.com";
+// let password = 1234;
+
+// let inputval="gohildhruvi1685@gmail.com";
+// let pass = 1352;
+
+// if(user!==inputval)
+// {
+//     if(password!=inputpass)
+//     {
+//         console.log("year password is incorect");
+//     }
+//     else{
+//         console.log("your user id incorect");
+//     }
+// }
+// else
+// {
+//     console.log("login successfull");
+    
+// }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                       
+
+
+
+
+
+
